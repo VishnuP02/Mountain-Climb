64912e8b39ac76729f50f2acd396da84539d33e4
Vishnu Pillai <135986853+VishnuP02@users.noreply.github.com>
2024-10-07 12:53:40 -0400

    Updated C++ project and added simulation code

---
+++ build/CMakeFiles/MountainClimb.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MountainClimb.dir/src/Main.cpp.o"
+  "CMakeFiles/MountainClimb.dir/src/Main.cpp.o.d"
+  "MountainClimb"
+  "MountainClimb.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MountainClimb.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
